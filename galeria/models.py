a81f88f5a1f78c29270e9ce462766667a4f49c83
hygorgiffoni <hygor.giffoni@gmail.com>
2024-01-22 11:07:02 -0300

    configuracao e crud admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,24 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Fotografia(models.Model):
+    '''Tabela dos dados relacionados a fotografias'''
+    CATEGORIAS = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta')
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=CATEGORIAS, default='')
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d', blank=True)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+
+    def __str__(self):
+        '''Retorna o nome do modelo'''
+        return self.nome
