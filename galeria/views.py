5acd6d7b2b1877a98dc169810276787109278ad4
hygorgiffoni <hygor.giffoni@gmail.com>
2024-01-24 07:59:22 -0300

    função de busca na página

--- galeria/views.py
+++ galeria/views.py
@@ -10,3 +10,14 @@ def imagem(request, foto_id):
     '''Função para imprimir na view o código html'''
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def buscar(request):
+    '''Função para imprimir na view o código html'''
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_buscar = request.GET['buscar']
+        if nome_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_buscar)
+
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
